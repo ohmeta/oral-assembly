3f2effe97780579feebdeb6689a4e9be0111a26b
alienzj <alienchuj@gmail.com>
2020-05-12 19:22:44 +0800

    update pipeline run script

--- pipeline/metapi.sh
+++ pipeline/metapi.sh
@@ -1,5 +1,19 @@
 #!/usr/bin/env bash
 
+# metapi
+# https://github.com/ohmeta/metapi
+
+# Installation
 conda install -c alienzj metapi
-metapi init -d ./ -s samples.tsv -b raw -a metaspades
-metapi run checkm_lineage_wf
+
+# Init project
+metapi init -d ./ -s samples.tsv -b trimming
+
+# see what metapi can do
+metapi denovo_wf --list
+
+# dry run
+metapi denovo_wf classify_all --dry_run
+
+# submmit jobs
+metapi denovo_wf classify_all --qsub
